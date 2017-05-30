f8c7c148a9d5d44fdb4260efb82814c6a8d22b3c
Jesse Geens <jesse.geens@gmail.com>
2017-05-30 20:30:15 +0200

    finished several expressions

--- asteroids/model/program/expression/ReadParameterExpression.java
+++ asteroids/model/program/expression/ReadParameterExpression.java
@@ -4,20 +4,31 @@ import java.util.List;
 
 import asteroids.model.program.Element;
 import asteroids.model.program.Expression;
+import asteroids.model.program.Function;
 import asteroids.part3.programs.SourceLocation;
 
-public class ReadParameterExpression extends Element implements Expression{
+public class ReadParameterExpression extends Element implements Expression<Object>{
 
-	protected ReadParameterExpression(SourceLocation sourceLocation) {
+	private String parameter;
+
+	protected ReadParameterExpression(SourceLocation sourceLocation, String paramName) {
 		super(sourceLocation);
-		// TODO Auto-generated constructor stub
+		setParameter(paramName);
+	}
+
+	public String getParameter() {
+		return this.parameter;
+	}
+
+	public void setParameter(String parameter) {
+		this.parameter = parameter;
 	}
 
 	@Override
-	public Object calculate() throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+	public Object calculate() {
+		throw new IllegalArgumentException();
 	}
+
 
 	@Override
 	public Object calculate(List actualArgs) throws IllegalArgumentException {
